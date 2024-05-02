bb20eaf2422f21a279983744be0934e0d4546923
rikyiso01 <riky.isola@gmail.com>
2024-05-02 09:09:21 +0200

    add formatter and fix upload

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -21,32 +21,34 @@
 
 #include "WiFiS3.h"
 
-// #include "arduino_secrets.h" 
+// #include "arduino_secrets.h"
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "test1234";        // your network SSID (name)
-char pass[] = "test1234";        // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key index number (needed only for WEP)
+char ssid[] = "test1234"; // your network SSID (name)
+char pass[] =
+    "test1234";   // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0; // your network key index number (needed only for WEP)
 
-int led =  LED_BUILTIN;
+int led = LED_BUILTIN;
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
 void setup() {
-  //Initialize serial and wait for port to open:
+  // Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   Serial.println("Access Point Web Server");
 
-  pinMode(led, OUTPUT);      // set the LED pin mode
+  pinMode(led, OUTPUT); // set the LED pin mode
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     // don't continue
-    while (true);
+    while (true)
+      ;
   }
 
   String fv = WiFi.firmwareVersion();
@@ -56,7 +58,7 @@ void setup() {
 
   // by default the local IP address will be 192.168.4.1
   // you can override it with the following:
-  WiFi.config(IPAddress(192,48,56,2));
+  WiFi.config(IPAddress(192, 48, 56, 2));
 
   // print the network name (SSID);
   Serial.print("Creating access point named: ");
@@ -67,7 +69,8 @@ void setup() {
   if (status != WL_AP_LISTENING) {
     Serial.println("Creating access point failed");
     // don't continue
-    while (true);
+    while (true)
+      ;
   }
 
   // wait 10 seconds for connection:
@@ -80,9 +83,8 @@ void setup() {
   printWiFiStatus();
 }
 
-
 void loop() {
-  
+
   // compare the previous status to the current status
   if (status != WiFi.status()) {
     // it has changed update the variable
@@ -92,57 +94,63 @@ void loop() {
       // a device has connected to the AP
       Serial.println("Device connected to AP");
     } else {
-      // a device has disconnected from the AP, and we are back in listening mode
+      // a device has disconnected from the AP, and we are back in listening
+      // mode
       Serial.println("Device disconnected from AP");
     }
   }
-  
-  WiFiClient client = server.available();   // listen for incoming clients
+
+  WiFiClient client = server.available(); // listen for incoming clients
 
   Serial.println("Looping");
 
-  if (client) {                             // if you get a client,
-    Serial.println("new client");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      delayMicroseconds(10);                // This is required for the Arduino Nano RP2040 Connect - otherwise it will loop so fast that SPI will never be served.
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out to the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+  if (client) {                   // if you get a client,
+    Serial.println("new client"); // print a message out the serial port
+    String currentLine =
+        ""; // make a String to hold incoming data from the client
+    while (client.connected()) { // loop while the client's connected
+      delayMicroseconds(
+          10); // This is required for the Arduino Nano RP2040 Connect -
+               // otherwise it will loop so fast that SPI will never be served.
+      if (client.available()) { // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c);        // print it out to the serial monitor
+        if (c == '\n') {        // if the byte is a newline character
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+          // if the current line is blank, you got two newline characters in a
+          // row. that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200
+            // OK) and a content-type so the client knows what's coming, then a
+            // blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println();
 
             // the content of the HTTP response follows the header:
-            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/H\">here</a> turn the LED on<br></p>");
-            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/L\">here</a> turn the LED off<br></p>");
+            client.print("<p style=\"font-size:7vw;\">Click <a "
+                         "href=\"/H\">here</a> turn the LED on<br></p>");
+            client.print("<p style=\"font-size:7vw;\">Click <a "
+                         "href=\"/L\">here</a> turn the LED off<br></p>");
 
             // The HTTP response ends with another blank line:
             client.println();
             // break out of the while loop:
             break;
-          }
-          else {      // if you got a newline, then clear currentLine:
+          } else { // if you got a newline, then clear currentLine:
             currentLine = "";
           }
-        }
-        else if (c != '\r') {    // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') { // if you got anything else but a carriage
+                                // return character,
+          currentLine += c;     // add it to the end of the currentLine
         }
 
         // Check to see if the client request was "GET /H" or "GET /L":
         if (currentLine.endsWith("GET /H")) {
-          digitalWrite(led, HIGH);               // GET /H turns the LED on
+          digitalWrite(led, HIGH); // GET /H turns the LED on
         }
         if (currentLine.endsWith("GET /L")) {
-          digitalWrite(led, LOW);                // GET /L turns the LED off
+          digitalWrite(led, LOW); // GET /L turns the LED off
         }
       }
     }
@@ -165,6 +173,4 @@ void printWiFiStatus() {
   // print where to go in a browser:
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
-
 }
-
