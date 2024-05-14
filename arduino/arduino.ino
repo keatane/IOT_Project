27fa026505de9dd2fc1a2f70c62fc88ec402ee66
rikyiso01 <riky.isola@gmail.com>
2024-05-14 13:13:59 +0200

    adding web server

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,35 +1,177 @@
-#include <Wire.h>
-// #include <LiquidCrystal_I2C.h>
+/*
+  WiFi Web Server LED Blink
 
-// LiquidCrystal_I2C lcd(0x27, 20, 4); // LCD2004
+  A simple web server that lets you blink an LED via the web.
+  This sketch will create a new access point (with no password).
+  It will then launch a new server and print out the IP address
+  to the Serial Monitor. From there, you can open that address in a web browser
+  to turn on and off the LED on pin 13.
 
-volatile int NbTopsFan; // Measuring the rising edges of the signal
-const double C=1/(60*10); // litres/spin
+  If the IP address of your board is yourAddress:
+    http://yourAddress/H turns the LED on
+    http://yourAddress/L turns it off
 
-const int hallsensor = 2; // The pin location of the sensor: D2
+  created 25 Nov 2012
+  by Tom Igoe
+  adapted to WiFi AP by Adafruit
 
-void rpm() { // Interrupt service routine (ISR) for rising edge
-  NbTopsFan++;
-}
+  Find the full UNO R4 WiFi Network documentation here:
+  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#access-point
+ */
+
+#include "WiFiS3.h"
+
+// #include "arduino_secrets.h"
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "caraffa_1"; // your network SSID (name)
+
+int led = LED_BUILTIN;
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
 
 void setup() {
+  // Initialize serial and wait for port to open:
   Serial.begin(9600);
-  pinMode(hallsensor, INPUT);
-  attachInterrupt(digitalPinToInterrupt(hallsensor), rpm, RISING); // Attach interrupt
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Access Point Web Server");
+
+  pinMode(led, OUTPUT); // set the LED pin mode
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true)
+      ;
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // by default the local IP address will be 192.168.4.1
+  // you can override it with the following:
+  WiFi.config(IPAddress(192, 168, 4, 1));
+
+  // print the network name (SSID);
+  Serial.print("Creating access point named: ");
+  Serial.println(ssid);
+
+  // Create open network. Change this line if you want to create an WEP network:
+  status = WiFi.beginAP(ssid);
+  if (status != WL_AP_LISTENING) {
+    Serial.println("Creating access point failed");
+    // don't continue
+    while (true)
+      ;
+  }
+
+  // wait 10 seconds for connection:
+  delay(10000);
+
+  // start the web server on port 80
+  server.begin();
+
+  // you're connected now, so print out the status
+  printWiFiStatus();
 }
 
 void loop() {
-  // NbTopsFan = 0; // Reset NbTopsFan for new measurement
 
-  // sei();        // Enable interrupts
-  delay(1000);  // Wait for 1 second (measurement period)
-  // cli();        // Disable interrupts
+  // compare the previous status to the current status
+  if (status != WiFi.status()) {
+    // it has changed update the variable
+    status = WiFi.status();
 
-  // Calc = (NbTopsFan * 60) / 7.5; // Calculate flow rate (L/hour)
+    if (status == WL_AP_CONNECTED) {
+      // a device has connected to the AP
+      Serial.println("Device connected to AP");
+    } else {
+      // a device has disconnected from the AP, and we are back in listening
+      // mode
+      Serial.println("Device disconnected from AP");
+    }
+  }
 
-  Serial.print(NbTopsFan, DEC);
-  Serial.println(" spins");
-  Serial.print(NbTopsFan*C, DEC);
-  Serial.println(" litres");
+  WiFiClient client = server.available(); // listen for incoming clients
+
+  // Serial.println("Looping");
+
+  if (client) {                   // if you get a client,
+    Serial.println("new client"); // print a message out the serial port
+    String currentLine =
+        ""; // make a String to hold incoming data from the client
+    while (client.connected()) { // loop while the client's connected
+      delayMicroseconds(
+          10); // This is required for the Arduino Nano RP2040 Connect -
+               // otherwise it will loop so fast that SPI will never be served.
+      if (client.available()) { // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c);        // print it out to the serial monitor
+        if (c == '\n') {        // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a
+          // row. that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200
+            // OK) and a content-type so the client knows what's coming, then a
+            // blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("<p style=\"font-size:7vw;\">Click <a "
+                         "href=\"/H\">here</a> turn the LED on<br></p>");
+            client.print("<p style=\"font-size:7vw;\">Click <a "
+                    "href=\"/L\">here</a> turn the LED off<br></p>");
+            client.print("<form method='post'><input name='ciao'><input type='submit'></form>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          if(currentLine.endsWith("")){
+
+              }
+            currentLine = "";
+        } else if (c != '\r') { // if you got anything else but a carriage
+                                // return character,
+          currentLine += c;     // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          Serial.println(currentLine);
+          digitalWrite(led, HIGH); // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(led, LOW); // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
 
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+}
